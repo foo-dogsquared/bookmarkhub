8efb1a5eac88876f4f776be44b0af8c095c70fa5
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-01-02 14:41:12 +0800

    Improve the test suite

--- test/parser/parser_netscape.js
+++ test/parser/parser_netscape.js
@@ -1,23 +1,24 @@
-const assert = require('assert');
+const assert = require("assert");
 const fs = require("fs");
+const mocha = require("mocha");
 const parser_src = require('../../src/parser/parser_netscape');
 
 const complete_netscape_obj = {
     "title": "Bookmarks", 
     "type": "netscape", 
-    "root_name": "Bookmarks menu", 
+    "root_name": "Bookmarks Menu", 
     "children": {
-        "Bookmarks bar": {
-            "Most Visited": "place:sort=8&maxResults=10",
-            "Getting Started": "https://www.mozilla.org/en-GB/firefox/central/"
-        },
+        "Recent Tags": "place:type=6&sort=14&maxResults=10",
         "Mozilla Firefox": {
             "Help and Tutorials": "https://support.mozilla.org/en-GB/products/firefox",
-            "Customize Firefox": "https://support.mozilla.org/en-GB/kb/customize-firefox-controls-buttons-and-toolbars?utm_source=firefox-browser&utm_medium=default-bookmarks&utm_campaign=customize",
+            "Customise Firefox": "https://support.mozilla.org/en-GB/kb/customize-firefox-controls-buttons-and-toolbars?utm_source=firefox-browser&utm_medium=default-bookmarks&utm_campaign=customize",
             "Get Involved": "https://www.mozilla.org/en-GB/contribute/",
             "About Us": "https://www.mozilla.org/en-GB/about/"
         },
-        "Recent Tags": "place:type=6&sort=14&maxResults=10"
+        "Bookmarks Toolbar": {
+            "Most Visited": "place:sort=8&maxResults=10",
+            "Getting Started": "https://www.mozilla.org/en-GB/firefox/central/"
+        }
     }
 };
 
@@ -43,15 +44,16 @@ describe(`Parsing through a Netscape bookmark HTML file with common cases`, func
     });
 
     it(`should detect the children of the root`, function() {
-        assert.deepStrictEqual(Object.getOwnPropertyNames(parser_src.parse_netscape(bookmark_text)).length, 3);
+        assert.deepStrictEqual(Object.getOwnPropertyNames(parser_src.parse_netscape(bookmark_text).children).length, 3);
     });
 
     it(`should create the object of the HTML body correctly`, function() {
-        assert.deepStrictEqual(parser_src.parse_netscape(bookmark_text).children, complete_netscape_obj.children);
+        const actual_results = JSON.stringify(parser_src.parse_netscape(bookmark_text).children);
+        const test_results = JSON.stringify(complete_netscape_obj.children);
+        assert.deepStrictEqual(actual_results, test_results);
     });
 
     it(`should return an object with the bookmarks from the generated HTML from bookmarks`, function() {
         assert.deepStrictEqual(parser_src.parse_netscape(bookmark_text), complete_netscape_obj);
     });
-
 })
