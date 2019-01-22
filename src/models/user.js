e61440cde5ef556ba8b409d20de0c689dad8c84d
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-01-22 18:51:19 +0800

    Update user schema with the verified status and more password validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bookmarks_schema = require("./bookmarks");
 const app_constants = require("../app_constants");
+const email_validator = require("email-validator");
 try {
     var crypto = require("crypto");
 } catch(err) {
@@ -17,7 +18,12 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     description: {
-        type: String
+        type: String,
+        trim: true
+    },
+    confirmed: {
+        type: Boolean,
+        default: false
     },
     salt: {
         type: String
@@ -29,13 +35,17 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Virtuals (getter and setter)
-UserSchema.virtual("password")
+UserSchema.virtual(app_constants.app_configuration.form_fields.password)
     .get(function() { return this._password })
     .set(function(password) {
         this._password = password;
         this.salt = this.generate_salt(16);
         this.hashed_password = this.hash_password(this._password, this.salt);
-    });
+});
+
+UserSchema.virtual(app_constants.app_configuration.form_fields.confirm_password)
+    .get(function() { return this._confirm_password })
+    .set(function(confirm_password) {this._confirm_password = confirm_password;});
 
 // Validations
 UserSchema.path("username").validate(function(username) {
@@ -88,20 +98,35 @@ UserSchema.path("email_address").validate(function(email_address_name) {
     else return true;
 }, app_constants.signup_error.BLANK_EMAIL_ADDRESS_SIGNUP_ERROR_MSG)
 
+UserSchema.path("email_address").validate(function(email_address_name) {
+    if (email_validator.validate(email_address_name)) return true;
+    else return false;
+}, app_constants.signup_error.EMAIL_ADDRESS_VALIDATION_SIGNUP_ERROR_MSG)
+
 UserSchema.path("description").validate(function(description) {
-    if (description.length >= app_constants.description_maxlength) return false;
+    if (description.length > app_constants.description_maxlength) return false;
     else return true;
-}, app_constants.general_error.DESCRIPTION_LENGTH_ERROR)
+}, app_constants.general_error.DESCRIPTION_LENGTH_ERROR);
+
+UserSchema.path("description").validate(function(description) {
+    if (description.match(/\S+/gi)) return true;
+    else return false;
+}, app_constants.general_error.DESCRIPTION_INVALID_ERROR_MSG);
 
 UserSchema.path("hashed_password").validate(function(hashed_password) {
-    if (this._password.length <= 0 && !hashed_password) return false;
+    if (this._password.length <= 0) return false;
     else return true;
 }, app_constants.signup_error.BLANK_PASSWORD_SIGNUP_ERROR_MSG);
 
 UserSchema.path("hashed_password").validate(function(hashed_password) {
-    if (this._password.length < 8 && !hashed_password) return false;
+    if (this._password.length < 8) return false;
     else return true;
 }, app_constants.signup_error.MINIMUM_PASSWORD_LENGTH_SIGNUP_ERROR_MSG)
+
+UserSchema.path("hashed_password").validate(function(hashed_password) {
+    if (this._password !== this._confirm_password) return false;
+    else return true;
+}, app_constants.signup_error.CONFIRM_PASSWORD_DOES_NOT_MATCH_INPUT_PASSWORD_SIGNUP_ERROR_MSG);
 
 // Methods
 UserSchema.method("generate_salt", function(bits) {
